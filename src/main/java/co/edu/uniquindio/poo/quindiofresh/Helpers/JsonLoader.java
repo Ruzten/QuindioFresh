4028f2d16cdf98a2f8c0d143a75109cf40f020c1
Jhonatan-Leon <c.jhonatan2007@gmail.com>
2025-09-14 22:39:30 -0500

    bug: Cambios en el lectos del JSON(valor quemado

--- src/main/java/co/edu/uniquindio/poo/quindiofresh/Helpers/JsonLoader.java
+++ src/main/java/co/edu/uniquindio/poo/quindiofresh/Helpers/JsonLoader.java
@@ -11,16 +11,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class JsonLoader {
-    // The method is now generic with <T> and returns a List<T>
     public static <T> List<T> CargarProductos(String fileName, Class<T> clazz) {
         try (var inputStream = JsonLoader.class.getClassLoader().getResourceAsStream(fileName)) {
-            if (inputStream == null) {
-                System.out.println("El archivo " + fileName + " no se encontró en el classpath.");
-                return new ArrayList<>(); // Returns an empty list, not null
-            }
             try (var reader = new InputStreamReader(inputStream)) {
                 Gson gson = new Gson();
-                // This gets the specific List<T> type at runtime
                 Type listType = TypeToken.getParameterized(List.class, clazz).getType();
                 return gson.fromJson(reader, listType);
             }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,10 @@ module co.edu.uniquindio.poo.quindiofresh {
     requires javafx.controls;
     requires javafx.fxml;
     requires com.google.gson;
+    requires jakarta.mail;
 
 
     opens co.edu.uniquindio.poo.quindiofresh to javafx.fxml;
-    opens co.edu.uniquindio.poo.quindiofresh.Model to javafx.fxml;
     exports co.edu.uniquindio.poo.quindiofresh.Model.ClasesConcretas;
-    opens co.edu.uniquindio.poo.quindiofresh.Model.ClasesConcretas to javafx.fxml;
+    opens co.edu.uniquindio.poo.quindiofresh.Model.ClasesConcretas to javafx.fxml, com.google.gson;
 }
